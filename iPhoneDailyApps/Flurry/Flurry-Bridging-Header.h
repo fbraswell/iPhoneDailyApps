36d7ea0b7e0147eaa12614b6e414c7c1aa9d0ef2
fbraswell <github@systemsofmerritt.com>
2015-05-31 01:02:46 -0400

    Almost ready for release

---
+++ iPhoneDailyApps/Flurry/Flurry-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Flurry-Bridging-Header.h
+//  iPhoneDailyApps
+//
+//  Created by Frank Braswell on 5/28/15.
+//  Copyright (c) 2015 Systems of Merritt, Inc. All rights reserved.
+//
+
+// Bridging header information for Flurry
+// http://stackoverflow.com/questions/25822222/how-to-integrate-flurry-in-ios-using-swift-language
+
+#ifndef iPhoneDailyApps_Flurry_Bridging_Header_h
+#define iPhoneDailyApps_Flurry_Bridging_Header_h
+
+#import "Flurry.h"
+
+#endif
